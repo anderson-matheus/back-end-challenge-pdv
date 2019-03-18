90725da7434a044d24ea8bc5354fbd1a955a9b2c
anderson-matheus <matheus5347@gmail.com>
2019-03-17 22:36:05 -0300

    Refatorando classe responsável por validar os campos para pesquisa de pdvs (pontos de venda) mais próximos

--- app/requests/search_nearest_request.py
+++ app/requests/search_nearest_request.py
@@ -1,14 +1,15 @@
 from wtforms import Form, StringField, validators, FloatField
 from wtforms.validators import ValidationError
-from app.utils.util import Util
+from app.utils.point_util import PointUtil
 
 class SearchNearestRequest(Form):
     coordinate = StringField('coordinate', [
         validators.DataRequired()
     ])
+
     def validate_coordinate(form, field):
-        util = Util()
-        coordinate = util.convert_point(field.data)
+        point_util = PointUtil()
+        coordinate = point_util.convert_point(field.data)
         if not coordinate.is_valid:
             raise ValidationError("This field is invalid")
 
