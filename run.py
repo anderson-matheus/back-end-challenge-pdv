93f946db9e94f07527263e83a081657479937106
anderson-matheus <matheus5347@gmail.com>
2019-03-17 22:38:55 -0300

    Implementando métodos do controller de pdv nas rotas

--- run.py
+++ run.py
@@ -2,11 +2,7 @@ from flask import Flask, request, jsonify
 from app.requests.store_pdv_request import StorePdvRequest
 from app.requests.get_by_id_request import GetByIdRequest
 from app.requests.search_nearest_request import SearchNearestRequest
-from app.utils.util import Util
-from app.models.pdv import Pdv
-from app.database import Database
-import bson
-import simplejson
+from app.controllers.pdv_controller import PdvController
 
 app = Flask(__name__)
 
@@ -14,51 +10,44 @@ app = Flask(__name__)
 def create():
     inputs = StorePdvRequest(request.form)
     inputs.set_coverage_area(request.form.getlist('coverage_area[]'))
-    util = Util()
     if not inputs.validate():
         return jsonify(success=False, errors=inputs.errors)
 
-    multi_polygon = util.convert_multi_polygon(request.form.getlist('coverage_area[]'))
-    document = request.form.get('document')
-    point = util.convert_point(request.form.get('address'))
-
-    pdv = Pdv(
-        document=util.format_document(document),
+    pdv = PdvController()
+    pdv = pdv.store(
+        document=request.form.get('document'),
         owner_name=request.form.get('owner_name'),
         trading_name=request.form.get('trading_name'),
-        coverage_area=multi_polygon,
-        address=point
+        coverage_area=request.form.getlist('coverage_area[]'),
+        address=request.form.get('address')
     )
-    pdv = pdv.save()
-    return jsonify(success=True, data=pdv.to_json())
+
+    return jsonify(success=True, data=pdv)
 
 @app.route('/get-by-id', methods=['GET'])
 def get_by_id():
-    mongodb = Database().get_connection()
     inputs = GetByIdRequest(request.args)
 
     if not inputs.validate():
         return jsonify(success=False, errors=inputs.errors)
 
-    pdv =  Pdv.objects(id=bson.objectid.ObjectId(request.args.get('id')))[0]
-    return jsonify(success=True, data=pdv.to_json())
+    pdv = PdvController()
+    pdv = pdv.get_by_id(request.args.get('id'))
+
+    return jsonify(success=True, data=pdv)
 
 @app.route('/search-nearest', methods=['GET'])
 def search_nearest():
-    mongodb = Database().get_connection()
     inputs = SearchNearestRequest(request.args)
-    util = Util()
     if not inputs.validate():
         return jsonify(success=False, errors=inputs.errors)
 
-    coordinate = util.convert_point(request.args.get('coordinate'))
-    pdvs =  Pdv.objects(address__near=coordinate,
-        address__max_distance=float(request.args.get('max_distance')))
-    data = list()
-    for pdv in pdvs:
-        data.append(pdv.to_json())
+    pdv = PdvController()
+    pdvs = pdv.search_nearest(
+        coordinate=request.args.get('coordinate'),
+        max_distance=request.args.get('max_distance'))
 
-    return jsonify(success=True, data=data)
+    return jsonify(success=True, data=pdvs)
 
 if (__name__ == "__main__"):
     app.run(port=5000)
