563c80fc45d56f08d820c4822c170285747c73bf
anderson-matheus <matheus5347@gmail.com>
2019-03-17 22:35:14 -0300

    Refatorando classe responsável por validar os campos para cadastro de pdv (ponto de venda)

--- app/requests/store_pdv_request.py
+++ app/requests/store_pdv_request.py
@@ -1,13 +1,12 @@
 from wtforms import Form, StringField, validators, FieldList, FormField, TextField
 from wtforms.validators import ValidationError
-from app.utils.util import Util
-
-util = Util()
+from app.utils.document_util import DocumentUtil
+from app.utils.point_util import PointUtil
+from app.utils.multi_polygon_util import MultiPolygonUtil
 
 class StorePdvRequest(Form):
     trading_name = StringField('trading name', [
         validators.DataRequired(),
-
     ])
 
     owner_name = StringField('owner name', [
@@ -18,20 +17,25 @@ class StorePdvRequest(Form):
         validators.DataRequired(),
     ])
     def validate_document(self, field):
-        if not util.document_is_unique(field.data) or not util.document_is_valid(field.data):
+        document_util = DocumentUtil()
+        if (not document_util.document_is_unique(field.data) or
+                not document_util.document_is_valid(field.data)):
             raise ValidationError("This field is invalid or duplicated")
 
     address = StringField('address', [
         validators.DataRequired(),
     ])
     def validate_address(form, field):
-        point = util.convert_point(field.data)
+        point_util = PointUtil()
+        point = point_util.convert_point(field.data)
         if not point.is_valid:
             raise ValidationError("This field is invalid or not exists")
 
     coverage_area = FieldList(StringField('coverage area', []))
     def validate_coverage_area(self, field):
-        multi_polygon = util.convert_multi_polygon(self.coverage_area)
+        multi_polygon_util = MultiPolygonUtil()
+        multi_polygon = multi_polygon_util.convert_multi_polygon(
+            self.coverage_area)
         if len(self.coverage_area) == 0:
             raise ValidationError("This field is required")
 
